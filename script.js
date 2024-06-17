3031da93b82fe663888fa45e5c95eb669e07b6c9
vishvanathan030303 <vishvanathan030303@gmail.com>
2024-06-17 14:06:24 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,41 @@
+/* 
+const a=1000;
+
+const result=a>=18?"Eligible":"Not Eligible";
+
+console.log(result)
+
+
+function Welcome(name){
+    const result=name?name:"No Name";
+    console.log("Welcome "+result);
+}
+Welcome("vishva")
+Welcome(null)
+
+id={'name':'Pooja','age':22}
+console.log(id)
+
+
+
+const wlcome=()=>{
+    const age=id?id.name:"No Name"
+    return "Hello "+age;
+}
+console.log(wlcome())
+*/
+
+
+// Conditional Chains
+var mark=80;
+const grade=mark>90?"A Grade":mark>80?"B Grade":"C Grade";
+console.log("Grade:",grade)
+
+
+//Bitwise Operator
+
+
+//
+
+
+git clone git@github.com:asktechjedi/content-generator-frontend.git
